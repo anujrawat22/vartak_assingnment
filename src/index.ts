de32c4788725c055da7130b00cd8af735c33a603
anujrawat22 <anuj22rawat20@gmail.com>
2023-07-07 12:48:44 +0530

    usermodel created

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { connection } from './config/db'
+import express from "express";
+import dotenv from "dotenv";
+import { connection } from "./config/db";
 
+dotenv.config();
 
+const app = express();
+const port = process.env.PORT;
 
-dotenv.config()
+app.use(express.json());
 
-const app = express()
-const port = process.env.PORT
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
 
-app.use(express.json())
-
-
-
-
-app.listen(port,async()=>{
-    try {
-        await connection;
-        console.log("Connected to DB");
-        
-        console.log(`Listening on PORT ${port}`)
-    } catch (error) {
-        
-    }
-})
+    console.log(`Listening on PORT ${port}`);
+  } catch (error) {
+    console.log(error)
+  }
+});
---
+++ src/models/userModel.ts
@@ -0,0 +1,24 @@
+import mongoose , {Schema , Document} from "mongoose";
+
+export interface IUser extends Document {
+    username : string;
+    email  : string;
+    password : string;
+    roles : string[];
+}
+
+export enum UserRole {
+    VIEWER = "VIEWER",
+    CREATOR = "CREATOR",
+    VIEW_ALL = "VIEW_ALL"
+}
+
+const userSchema : Schema = new Schema({
+    username : {type : String,required : true},
+    email : {type : String , required : true},
+    password : {type : String, required : true},
+    roles : [{type : String , enum : Object.values(UserRole) , default : UserRole.VIEWER}]
+})
+
+
+export const User = mongoose.model<IUser>('User',userSchema)
